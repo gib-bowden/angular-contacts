96b100c99fce25c349629b84577001d0a6805c33
gib <gibbowden@gmail.com>
2017-11-16 18:24:47 -0600

    validation and submit shell function working

--- javascripts/controllers/contacts/NewCtrl.js
+++ javascripts/controllers/contacts/NewCtrl.js
@@ -1,5 +1,15 @@
 "use strict"; 
 
 app.controller("NewCtrl", function($scope){
-    $scope.controller = "NewCtrl"; 
+
+    // function to submit the form after all validation has occurred			
+    $scope.submitForm = ()  => {
+    // check to make sure the form is completely valid
+    if ($scope.newContactForm.$valid) {
+        console.log($scope.contact);
+        $scope.newContactForm.$setUntouched();
+        $scope.contact={};
+    }
+    };
+
 }); 
